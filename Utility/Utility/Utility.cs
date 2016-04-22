4c00640b81d3d68dc0b75fa2fa0b0b15a0ad268e
Eason <eason_wang@ebc.net.tw>
2016-04-22 10:05:25 +0800

    namespace變更

--- Utility/Utility/Utility.cs
+++ Utility/Utility/Utility.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;   //捉 Server IP 用
 using System.Web;
 
 //網站共用功能函式庫
-namespace tw.net.ebc {
+namespace common.tools.function {
 
     /// <summary>
     /// 網站共用的功能
